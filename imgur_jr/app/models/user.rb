c8c1d0b5537af5a11af9463f683a5c97f3d46323
Alan Florendo & Justin Lee <pair+alanflorendo.leeju@devbootcamp.com>
2014-08-28 21:34:04 -0400

    Generate User model, write tests for User validations

---
+++ imgur_jr/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  validates :email, presence: true
+  validates :password, presence: true
+
+  has_many :photos
+
+  has_secure_password
+end
--- imgur_jr/db/migrate/20140829001302_create_photos.rb
+++ imgur_jr/db/migrate/20140829001302_create_photos.rb
@@ -3,6 +3,7 @@ class CreatePhotos < ActiveRecord::Migration
     create_table :photos do |t|
       t.string :title
       t.string :url
+      t.integer :user_id
 
       t.timestamps
     end

d21fee038637487e93680601ba54ee1dcef6121e
Alan Florendo & Justin Lee <pair+alanflorendo.leeju@devbootcamp.com>
2014-08-28 21:16:22 -0400

    Write tests for PhotosController

--- imgur_jr/spec/models/photo_spec.rb
+++ imgur_jr/spec/models/photo_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe Photo do
-  it {should have_many(:answers)}
   it {should validate_presence_of(:title)}
-  it {should validate_presence_of(:content)}
+  it {should validate_presence_of(:url)}
 end

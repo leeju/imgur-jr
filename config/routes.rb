0f6eae11c04570bd24f092bc62f92255d10ca6b5
Eric Shemtov <shemtov.eric@gmail.com>
2014-08-29 10:37:25 -0400

    added controllers for sessions and users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'photos#index'
 
   resources :photos
+  resources :users, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end

68af9923c1cb42a7eb8d9430a4c54358ed7a8689
Stephen Estrada <stcrestrada@gmail.com>
2014-08-29 15:53:21 -0400

    valid user email params for user rspec model rpsec test success

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 describe User do
-  it {should have_many(:photos)}
-  it {should validate_presence_of(:email)}
-  it {should validate_presence_of(:password)}
+	let!(:user) { User.create(email:"stephen@gmail.com", password:"stephen") }
+	
+	context "email" do 
+		it "should return valid email" do  
+			expect(user.email).to eq("stephen@gmail.com")
+		end
+	end
 end
+

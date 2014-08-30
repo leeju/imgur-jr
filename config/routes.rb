c09361aefce759fbb8558e81ca8f1ca085d6d9cb
alanflorendo <alan.florendo@gmail.com>
2014-08-30 18:36:57 -0400

    play with routes for nested polymorphic

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,19 @@ Rails.application.routes.draw do
 
   root 'photos#index'
 
-  resources :photos do
+  concern :votable do
     resources :votes, only: [:create, :update]
-    resources :comments do
-      resources :votes, only: [:create, :update]
-    end
+  end
+
+  resources :photos, concerns: :votable do
+    resources :comments, concerns: :votable
   end
   resources :users, only: [:new, :create] #:destroy]
 
   get 'login' => 'sessions#new', as: :login
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy', as: :logout
+
+
+
 end

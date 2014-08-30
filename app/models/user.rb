40933cf45b2c8fbcea2da0ae5889a8704836d56b
alanflorendo <alan.florendo@gmail.com>
2014-08-30 16:01:54 -0400

    Validate username
    Code and spec for
    username/validation (presence and uniqueness)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :email, presence: true
+  validates :username, presence: true
   validates :password, presence: true
   validates_uniqueness_of :email
   validates_confirmation_of :password, message: "Password doesn't match"

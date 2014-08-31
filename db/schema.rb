eb500983ecda7f0f28d3b88fd5e2901efa3fde5e
Eric Shemtov <shemtov.eric@gmail.com>
2014-08-31 00:22:16 -0400

    fixed if in nav bar, fixed css slightly, changed background image to reflect imgurs, fixed link on index view of images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831000947) do
+ActiveRecord::Schema.define(version: 20140829154114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,13 +27,6 @@ ActiveRecord::Schema.define(version: 20140831000947) do
 
   add_index "comments", ["photo_id"], name: "index_comments_on_photo_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-
-  create_table "images", force: true do |t|
-    t.string   "image_uid"
-    t.string   "image_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "photos", force: true do |t|
     t.string   "title"
